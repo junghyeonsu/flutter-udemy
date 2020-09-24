a4fbfda58529366f8142683950011d7947cf3948
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-09-24 16:53:26 +0900

    i am rich

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// main 함수는 모든 flutter app의 시작점입니다.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[200],
+        appBar: AppBar(
+          title: Center(child: Text("I am Rich")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/diamond.png')),
+        ),
+      ),
+    ),
+  );
+}
